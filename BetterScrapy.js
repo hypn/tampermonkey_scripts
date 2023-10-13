994af1ae85e7ebad8fff5fb7194ca0909c31feb3
Ross <github@hypn.za.net>
2023-10-13 23:16:59 +0200

    Added "Biggest Discount" sorting and made names clickable

--- BetterScrapy.js
+++ BetterScrapy.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BetterScrapy
 // @namespace    https://scrapy.co.za/
-// @version      0.1
+// @version      0.2
 // @description  Link directly to external sites, and sort by lowest price
 // @author       HypnZA
 // @match        https://scrapy.co.za/*
@@ -18,7 +18,6 @@
 
         if (elements.length) {
             let parent = elements[0].parentElement;
-            console.log("Deleting " + elements.length + " elements...");
 
             // sort them by price
             var array = [];
@@ -41,16 +40,64 @@
                     parent.appendChild(sorted[i].elem);
                 }
             }
+        }
+    }
 
-            // // force images to reload
-            // window.scrollBy(1,1);
-            // window.scrollBy(-1,-1);
+    window.sortByBiggestDiscount = function() {
+        // find all elements
+        let elements = document.getElementsByClassName("col-lg-3 col-md-4 col-sm-6 mb-grid-gutter");
+
+        if (elements.length) {
+            var parent = elements[0].parentElement;
+
+            // sort them by price
+            var array = [];
+            for (var i = elements.length - 1; i >= 0; i--) {
+                var elem = elements[i];
+
+                // let discount = elem.getElementsByClassName("daily-discount") || ;
+
+                let dailyDiscount = elem.getElementsByClassName("daily-discount");
+                let averageDiscount = elem.getElementsByClassName("average-discount");
+
+                let discount = "0";
+                if (averageDiscount.length > 0) {
+                  discount = averageDiscount[0].textContent.replace(/\s/g,'')
+                }
+                if (dailyDiscount.length > 0) {
+                  discount = dailyDiscount[0].textContent.split(":")[1].replace(/\s/g,'')
+                }
+
+                if (discount && (discount[0] == '-')) {
+                  discount = discount.replace("-", "").replace("%", "");
+                } else {
+                  discount = 0;
+                }
+
+                array.push({
+                    elem: elem,
+                    value: parseFloat(discount)
+                });
+                parent.removeChild(elem);
+            }
+
+            var sorted = array.sort(function(a, b) {
+                return (a.value > b.value) ? 1 : ((b.value > a.value) ? -1 : 0)
+            }).reverse();
+            console.log(sorted);
+
+            // add the results back
+            for (var i = 0; i < sorted.length; i++) {
+                if (parent) {
+                    parent.appendChild(sorted[i].elem);
+                }
+            }
         }
     }
 
     let running = false;
 
-    function fixLinks() {
+    function doStuff() {
         if (!running) {
             running = true;
 
@@ -63,19 +110,36 @@
                 item.removeAttribute("data-bs-toggle");
             }
 
+            // make names into links and disable truncation
+            Array.from(document.getElementsByClassName("product-title mb-2 fs-base")).forEach(elem => {
+              let name = elem.textContent;
+              let link = elem.parentElement.parentElement.getElementsByTagName("a")[0].href;
+              elem.innerHTML = '<a href="' + link + '">' + name + '</a>';
+            });
+
+            // add "sort" link(s)
             let topBar = document.getElementsByClassName("bg-light shadow-lg rounded-3 p-4 mt-n5 mb-4")[0];
             if (topBar) {
                 if (!document.getElementById("hypnSort1")) {
                     var lowestPrice = document.createElement("a");
                     lowestPrice.id = "hypnSort1";
                     lowestPrice.href = "javascript:window.sortByLowestPrice()";
-                    //lowestPrice.classList.add("lowestPrice");
-
-                    var myStrong = document.createElement("strong");
-                    myStrong.innerText = "Lowest Price"
-                    lowestPrice.appendChild(myStrong);
-
+                    var lowestPriceText = document.createElement("strong");
+                    lowestPriceText.innerText = "Lowest Price"
+                    lowestPrice.appendChild(lowestPriceText);
                     topBar.appendChild(lowestPrice);
+
+                    var spacer1 = document.createElement("span");
+                    spacer1.innerText = "  |  "
+                    topBar.appendChild(spacer1);
+
+                    var biggestDiscount = document.createElement("a");
+                    biggestDiscount.id = "hypnSort2";
+                    biggestDiscount.href = "javascript:window.sortByBiggestDiscount()";
+                    var biggestDiscountText = document.createElement("strong");
+                    biggestDiscountText.innerText = "Biggest Discount"
+                    biggestDiscount.appendChild(biggestDiscountText);
+                    topBar.appendChild(biggestDiscount);
                 }
             }
 
@@ -83,5 +147,5 @@
         }
     }
 
-    setInterval(fixLinks, 1000);
+    setInterval(doStuff, 1000);
 })();
